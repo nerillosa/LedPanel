00055bff90a3cf4dd67d9bc81419825092a934eb
Neri Llosa <nerillosa@gmail.com>
2016-11-06 22:04:55 +0000

    rssFeed

---
+++ parseXml.c
@@ -0,0 +1,107 @@
+/*
+ * C program that gets rss feed from Fox and extracts the titles
+ * There might be easier ways of doing this but this program
+ * creates two files: feed.xml and feed.txt
+ * The xml file is the direct feed from the host's website.
+ * The text file is the extracted titles, one on each line.
+ * To run this program you will need to install libCurl and Mini-XML version 2.9
+ * To make it work I had to copy (once installed) the mini-XML shared library (.so) from
+ * /usr/local/lib to /usr/lib because the compiler was not finding it.
+ *
+ * To compile: gcc -o parseXml parseXml.c -lmxml -lcurl -pthread
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <curl/curl.h>
+#include <mxml.h>
+
+void download_feed(FILE *dst, const char *src);
+int peekFile(FILE *dst);
+
+int main(int argc, char **argv)
+{
+	char *url = "http://feeds.foxnews.com/foxnews/world?format=xml";
+	if(argc > 1){
+		url = argv[1];
+	}
+
+    	FILE *fptr, *tptr;
+
+        fptr = fopen("feed.xml", "w");
+	download_feed(fptr, url);
+	fclose(fptr);
+
+	mxml_node_t *tree;
+
+	int whitespace = 0;
+	/*  open for writing */
+	fptr = fopen("feed.xml", "r");
+
+	if(peekFile(fptr) == -1){
+                printf("file is not an xml file: %s\n", url);
+		fclose(fptr);
+		return 1;
+	}
+
+	tptr = fopen("feed.txt", "w");
+
+	if (fptr == NULL || tptr == NULL){
+		printf("%s:Could not open file feed.txt \n", argv[0]);
+		return 1;
+	}
+
+	tree = mxmlLoadFile(NULL, fptr, MXML_TEXT_CALLBACK);
+
+	mxml_node_t *node, *titleNode;
+	node = mxmlFindElement(tree, tree, "item", NULL, NULL, MXML_DESCEND);
+
+	titleNode = mxmlFindElement(node, tree, "title", NULL, NULL, MXML_DESCEND);
+	titleNode = mxmlGetFirstChild(titleNode);
+	fprintf(tptr, "%s", mxmlGetText(titleNode, &whitespace));
+	while(1){
+		titleNode = mxmlGetNextSibling(titleNode);
+		if(titleNode == NULL) break;
+		fprintf(tptr, " %s", mxmlGetText(titleNode, &whitespace));
+	}
+	fprintf(tptr, "\n");
+
+	node = mxmlGetNextSibling(node);
+	while(node != NULL){
+		titleNode = mxmlFindElement(node, tree, "title", NULL, NULL, MXML_DESCEND);
+		titleNode = mxmlGetFirstChild(titleNode);
+		if(titleNode != NULL)
+			fprintf(tptr, "%s", mxmlGetText(titleNode, &whitespace));
+		while(1){
+			titleNode = mxmlGetNextSibling(titleNode);
+			if(titleNode == NULL) break;
+			fprintf(tptr, " %s", mxmlGetText(titleNode, &whitespace));
+		}
+		node = mxmlGetNextSibling(node);
+		node = mxmlGetNextSibling(node);
+		if(node != NULL)
+			fprintf(tptr, "\n");
+	}
+
+	fclose(fptr);
+	fclose(tptr);
+	return 0;
+}
+
+void download_feed(FILE *dst, const char *src){
+	CURL *handle = curl_easy_init();
+	curl_easy_setopt(handle, CURLOPT_URL, src);
+	curl_easy_setopt(handle, CURLOPT_WRITEDATA, dst);
+	curl_easy_perform(handle);
+}
+
+int peekFile(FILE *dst){ // check if its an xml file starting with <?
+	int c,d;
+	c = fgetc(dst);
+	if(c != '<') return -1;
+	d = fgetc(dst);
+	if(d != '?') return -1;
+
+	ungetc(d, dst);
+	ungetc(c, dst);
+	return 0;
+}
---
+++ rssFeedTicker.c
@@ -0,0 +1,220 @@
+// rssFeedTicker.c
+// Running this program shows FOX Rss news feeds scrolling from right to left. 
+// After installing bcm2835, you can build and run this with:
+// gcc -o rssFeedTicker -l rt rssFeedTicker.c lcdshapes.c -l bcm2835
+// sudo ./rssFeedTicker
+
+#include <sys/time.h>
+#include <stdio.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <stdint.h>
+
+#include "lcdshapes.h"
+#include "lcdfonts.h"
+
+#define MOVE_INTERVAL  30  // time in milliseconds between each move
+
+struct news {
+  void *titles;
+  int size;
+};
+
+static struct urls {
+ char name[10];
+ char *url;
+ struct urls *next;
+} *foxurls = NULL;
+
+void refreshFeed();
+void getNews(struct news *newsTitles);
+void paintCanvas(uint8_t *canvas);
+void getUrls();
+
+struct timeval saved, now;
+
+char **titles; // pointer to an array of strings
+char *mesg;
+struct news newsTitles;
+int numLines;
+int strlength;
+char* categories[] = {"health", "business", "national", "world", "latest", "politics", "scitech", "entertainment"};
+
+int main(int argc, char **argv)
+{
+	if (gpio_init())
+		return 1;
+
+	getUrls();
+	refreshFeed();
+	newsTitles.titles = NULL;
+	getNews(&newsTitles);
+	numLines = newsTitles.size;
+	titles = (char **)newsTitles.titles;
+        mesg = titles[0];
+	strlength = strlen(mesg);
+
+	uint8_t *canvas = (uint8_t *)calloc(PANEL_SIZE, sizeof(uint8_t));
+	gettimeofday(&saved, NULL); //start time
+	while (true) //infinite loop
+	{
+		displayRowInit();
+		paintCanvas(canvas);
+		updateRows(canvas);
+	}
+}
+
+void paintCanvas(uint8_t *canvas){
+	static int mesgCounter = 0;
+	static int lineCounter = 0;
+	static int moveOffset = NUMBER_PANELS * NUMBER_COLUMNS_PER_PANEL;
+	static color clr = blue;
+
+	gettimeofday(&now, NULL);
+
+        if(getTimeDiff(now, saved) > MOVE_INTERVAL){
+		timevalCopy(&saved, &now);
+		memset(canvas, 0, PANEL_SIZE); // clear the canvas
+		if(--moveOffset == -1*LETTER_WIDTH*strlength){ //message repeated itself
+			moveOffset = TOTAL_NUMBER_COLUMNS;
+			if(++mesgCounter == 1) { // message repeated 1 times. time for next title
+				mesgCounter = 0;
+				if(++clr == 8) clr = blue;
+				if(clr == red) clr++; //first row is always red
+				if(++lineCounter == numLines) { //refresh feed after all titles have scrolled
+					lineCounter = 0;
+					foxurls = foxurls -> next; //next category
+					refreshFeed();
+					getNews(&newsTitles);
+					numLines = newsTitles.size;
+					titles = (char **)newsTitles.titles;
+				}
+				mesg = titles[lineCounter];
+				strlength = strlen(mesg);
+			}
+		}
+		int i=0;
+		char *namee = foxurls -> name;
+		int namelen = strlen(namee);
+        	for(i=0;i<namelen;i++){
+              		drawLetter(*(namee+i), LETTER_WIDTH*i, 0, red, canvas);
+		}
+		char letra[10];
+		memset(letra, 0, 10);
+		letra[0] = ' ';
+		sprintf(&letra[1], "%d", numLines - lineCounter);
+		int k = 0;
+		for(k=0;k<strlen(letra);k++,i++){
+			drawLetter(*(letra+k), LETTER_WIDTH*i, 0, red, canvas);
+		}
+        	for(i=0;i<strlength;i++){
+              		drawLetter(*(mesg+i), moveOffset + LETTER_WIDTH*i, 9, clr, canvas);
+	      	}
+	}
+}
+
+void getNews(struct news *newsTitles){
+	int j = 0;
+	if(newsTitles -> titles != NULL){ //free all the allocated memory
+		char **titless = (char **)(newsTitles -> titles);
+		for(j=0;j<newsTitles -> size; j++){
+			free(titless[j]);
+		}
+		free(newsTitles -> titles);
+	}
+	char* fileName = "feed.txt";
+	FILE* file = fopen(fileName, "r");
+        if (file == NULL){
+                printf("Could not open feed file \n");
+                return;
+        }
+
+	char line[256];
+	memset(line, 0, 256);
+
+	char **news = malloc(100 * sizeof(char*)); // 100 titles of 256 chars each
+	int nlines = 0;
+
+	while (fgets(line, sizeof(line), file)) {
+		int i = 0;
+		while(line[i]){
+			line[i] = toupper(line[i]);
+			i++;
+		}
+		news[nlines] = malloc(strlen(line) +1);
+		strcpy(news[nlines++], line);
+	}
+
+	newsTitles -> titles = news;
+	newsTitles -> size = nlines;
+
+	fclose(file);
+}
+
+void refreshFeed(){
+	pid_t pid;
+	int status;
+	pid_t ret;
+	char *const args[3] = {"./parseXml", foxurls -> url, NULL};
+	//char **env;
+	extern char **environ;
+
+	pid = fork();
+	if (pid == -1) {
+		/* Handle error */
+		printf("fork failed\n");
+	} else if (pid != 0) { //this is the parent
+		while ((ret = waitpid(pid, &status, 0)) == -1) {
+			if (errno != EINTR) {
+				/* Handle error */
+				printf("errno != EINTR: %d\n", errno);
+				break;
+			}
+		}
+		if ((ret != -1) && (!WIFEXITED(status) || !WEXITSTATUS(status)) ) {
+			/* Report unexpected child status */
+			printf("Unexpected child status:%d\n", status);
+		}
+	} else { //this is the child
+		if (execve("./parseXml", args, NULL) == -1) {
+			/* Handle error */
+			printf("execve returned -1\n");
+			printf("next category because this category failed\n");
+			foxurls = foxurls -> next; //next category because this category failed
+			_Exit(127);
+		}
+	}
+
+//	char line[256];
+//	memset(line, 0, 256);
+//	strcpy(line, "sudo ./parseXml ");
+//	strcat(line, foxurls -> url);
+//	system(line);
+}
+
+void getUrls(){
+	int lenn = sizeof(categories)/sizeof(categories[0]);
+	foxurls = (struct urls *)calloc(lenn, sizeof(struct urls));
+	int i;
+	for(i=0;i<lenn;i++){
+		char line[256];
+		memset(line, 0, 256);
+		strcpy(line, "http://feeds.foxnews.com/foxnews/" );
+		strcat(line, categories[i]);
+		strcat(line, "?format=xml");
+		foxurls[i].url = malloc(strlen(line) +1);
+		strcpy(foxurls[i].url, line);
+		foxurls[i].next = &foxurls[(i+1)%lenn]; // circular linked list
+		int j = 0;
+		char *category = categories[i];
+                while(category[j] && j<6){ // max 6 chars to give space for numlines
+			foxurls[i].name[j] = toupper(category[j]);
+			j++;
+                }
+	}
+}
