099905adf7d9617919d17c9e8fb9b2a634b89fbf
Neri Llosa <nerillosa@gmail.com>
2016-11-13 23:12:09 +0000

    rssFeeds

--- parseXml.c
+++ parseXml.c
@@ -10,16 +10,25 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <ctype.h>
 #include <curl/curl.h>
+#include <sys/stat.h>
 #include "parseXml.h"
 
 static void download_feed(FILE *dst, const char *src);
-static int peekFile(FILE *dst);
+//static int peekFile(FILE *dst);
 static void getTitle(char *line);
+int fsize(const char *filename);
 
 int refreshFeed(char *url)
 {
     	FILE *fptr, *tptr;
+        char identifier[12];
+        memset(identifier, 0, 12);
+        char a;
+        int state=0;
+
+
 	/*  open for writing */
         fptr = fopen("feed.xml", "w");
 	if (fptr == NULL){
@@ -31,16 +40,17 @@ int refreshFeed(char *url)
 	download_feed(fptr, url);
 	fclose(fptr);
 
+        int fileSize = fsize("feed.xml");
+        if (fileSize == -1){
+                printf("%s:Could not stat size of feed.xml\n", "parseXml");
+                return 1;
+        }
+
+
 	/*  open for reading */
 	fptr = fopen("feed.xml", "r");
 	if (fptr == NULL){
 		printf("Could not open file feed.xml for reading \n");
-		return 1;
-	}
-
-	if(peekFile(fptr) == -1){
-                printf("file is not an xml file: %s\n", url);
-		fclose(fptr);
 		return 1;
 	}
 
@@ -51,19 +61,35 @@ int refreshFeed(char *url)
 		return 1;
 	}
 
-        char line[1024];
-	int j = 0;
-        while (fgets(line, sizeof(line), fptr)) {
-		int i = 0;
-                while(line[i]){
-	                line[i] = toupper(line[i]);
-        	        i++;
-              	}
-	        getTitle(line);
-        	if(strlen(line)){
-                	if(++j>2) fprintf(tptr, "%s\n", line); // skip the first two trivial title
-		}
-	}
+        int i=0,j=0;
+	int pos = 0;
+	char buffer[512];
+        do { //this do loop will find all the text in between title tags
+                a = fgetc(fptr);
+                int k;
+                for(k=1;k<11;k++){
+                        identifier[k-1] = identifier[k];
+                }
+                identifier[k-1]=a;
+                if(state==0){
+                        if(strstr(identifier, "<title>") != NULL){
+                                state=1;
+				pos=0;
+                        }
+                }
+                else if(state ==1){
+                        buffer[pos++] = a;
+                        if(strstr(identifier, "</title>") != NULL){
+                                state=0;
+				buffer[pos] = '\0';
+				if(++i>2){ // skip the first two titles
+					getTitle(buffer);
+					fprintf(tptr, "%s\n", buffer);
+				}
+                        }
+                }
+
+        } while (a != EOF && ++j<fileSize);// some files don't have EOF
 
 	fclose(fptr);
 	fclose(tptr);
@@ -77,28 +103,29 @@ void download_feed(FILE *dst, const char *src){
 	curl_easy_perform(handle);
 }
 
-int peekFile(FILE *dst){ // check if its an xml file starting with <?
-	int c,d;
-	c = fgetc(dst);
-	if(c != '<') return -1;
-	d = fgetc(dst);
-	if(d != '?') return -1;
+int fsize(const char *filename) {
+    struct stat st;
 
-	ungetc(d, dst);
-	ungetc(c, dst);
-	return 0;
+    if (stat(filename, &st) == 0)
+        return (st.st_size + 0);
+
+    return -1;
 }
 
+
 void getTitle(char *line){
-        static char *prefix = "<TITLE>";
         static char *suffix = "</TITLE>";
 	static char *cdata = "<![CDATA[";
-
-        char *p1 = strstr(line, prefix);
+	int i =0;
+	while(line[i]){
+		line[i] = toupper(line[i]);
+		i++;
+	}
+        char *p1 = &line[0];
         char *p2 = strstr(line, suffix);
-        int i;
-        if(p1) p1 += strlen(prefix) + (strstr(line, cdata)? strlen(cdata) : 0);
-        if(p1 && p2 && p2>p1){
+
+        p1 += (strstr(line, cdata)? strlen(cdata) : 0);
+        if(p2 && p2>p1){
                 for(i=0;i<(p2-p1);i++){
                    line[i] = *(p1+i);
                 }
@@ -106,5 +133,26 @@ void getTitle(char *line){
         }else{
                 line[0] = 0;
         }
+
+	int k=0;
+	char *bandido = NULL;
+
+	if(strstr(line, "WATCH:") == &line[0]){ // starts with WATCH:
+		bandido = strdup(&line[0]+6);
+		while(isspace(bandido[k])) k++;
+		strcpy(line, bandido+k);
+		free(bandido);
+		return;
+	}
+
+	while(isspace(line[k])) k++; //get rid of leading whitespace
+
+	if(k){
+		bandido = strdup(&line[0]+k);
+		strcpy(line, bandido);
+		free(bandido);
+	}
+
+
 }
 
